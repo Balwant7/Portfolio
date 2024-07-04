0fb76b067581c61e146f30944e5f1841a36803d5
Balwant Singh <singhbalwant168@gmail.com>
2024-07-04 12:31:41 +0530

    updated footer

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div id="footer" className="w-full bg-black h-[50vh] pt-20">
-      <div className="h-full w-full flex flex-col pt-[4rem] items-center ">
-        <div className="flex flex-col justify-center items-center w-full h-full px-10 ">
+      <div className="h-full w-full flex flex-col pt-[4rem] items-center justify-center">
+        <div className="flex flex-col justify-center items-center w-full h-full px-10 md:px-10 ">
           <h3 className="text-white  left">Have a project in mind?</h3>
-          <h1 className="text-white text-[10rem]">LET'S TALK</h1>
+          <h1 className="text-white text-[8rem] md:text-[10rem]">LET'S TALK</h1>
         </div>
       </div>
-      <div className="bg-black flex gap-8 p-10">
+      <div className="bg-black flex gap-8 p-5 pt-10">
         <Link to="https://github.com/Balwant7">
           <button className="bg-white rounded-full px-7 py-3">Github</button>
         </Link>
